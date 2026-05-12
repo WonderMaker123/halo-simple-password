dd612cef44ea7604f317d44e07b534f8e7704467
root <root@iZ7xv0q5feffswjzdm2p0kZ.debian>
2026-05-12 12:54:23 +0800

    Fix: use onStart() instead of onStartup(), use getLogger()

--- src/main/java/run/halo/simplepassword/SimplePasswordPlugin.java
+++ src/main/java/run/halo/simplepassword/SimplePasswordPlugin.java
@@ -1,17 +1,15 @@
 package run.halo.simplepassword;
 
-import lombok.extern.slf4j.Slf4j;
 import run.halo.app.plugin.BasePlugin;
 import run.halo.app.plugin.PluginContext;
 
-@Slf4j
 public class SimplePasswordPlugin extends BasePlugin {
     public SimplePasswordPlugin(PluginContext pluginContext) {
         super(pluginContext);
     }
 
     @Override
-    public void onStartup() {
-        log.info("Simple Password Plugin started!");
+    public void onStart() {
+        getLogger().info("Simple Password Plugin started!");
     }
 }
